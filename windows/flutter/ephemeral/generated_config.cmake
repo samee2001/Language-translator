4dd370638118ec9fd8765116fbbc7b61847f1b7c
Sameera Wijesekara <sameerawijesekara2001@gmail.com>
2024-12-13 20:25:18 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\University assignments\\Android\\language_translator" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "PROJECT_DIR=E:\\University assignments\\Android\\language_translator"
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\University assignments\\Android\\language_translator\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\University assignments\\Android\\language_translator"
+  "FLUTTER_TARGET=E:\\University assignments\\Android\\language_translator\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\University assignments\\Android\\language_translator\\.dart_tool\\package_config.json"
+)
